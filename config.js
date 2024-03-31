ab1bd71c0e43b53bbfb4f448686d1e7847d8b70e
Simo1234566 <154939651+Simo1234566@users.noreply.github.com>
2024-03-31 00:45:08 +0000

    Update config.js

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2341', '5491128630079', '2347045035241','233533763772'] 
-global.prems = ['5491128630079','233208179431','233533763772']
-global.allowed = ['5491128630079', '2347045035241']
+global.mods = ['5491128630079', '5491128630079', '5491128630079','5491128630079'] 
+global.prems = ['5491128630079','5491128630079','5491128630079']
+global.allowed = ['5491128630079', '5491128630079']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
